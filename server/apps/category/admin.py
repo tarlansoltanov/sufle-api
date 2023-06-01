c92661f3734212cf312c7e6e541d1f6de3f93e00
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-01 19:17:19 +0200

    Category Model added

--- server/apps/category/admin.py
+++ server/apps/category/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from server.apps.category.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'main_category',
+        'created_at',
+    )
--- server/apps/category/models.py
+++ server/apps/category/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Model definition for Category."""
+
+    name = models.CharField(max_length=255)
+    logo = models.FileField(upload_to='category/')
+    main_category = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='sub_categories'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Category."""
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        """Unicode representation of Category."""
+        return self.name
