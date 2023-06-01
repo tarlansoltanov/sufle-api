161fb999b1c6637cd9371911ff73f521071de512
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-01 21:30:57 +0200

    Category Model admin n+1 query fixed

--- server/apps/category/admin.py
+++ server/apps/category/admin.py
@@ -5,6 +5,7 @@ from server.apps.category.models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_select_related = ['main_category']
     list_display = (
         'id',
         'name',
