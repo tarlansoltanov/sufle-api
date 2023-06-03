7dfd9eb959b8c2a4c700cdf8fab44f8c42d2a048
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-03 15:07:06 +0200

    Category Read Serializer Sub Categories added

--- server/apps/category/logic/serializers.py
+++ server/apps/category/logic/serializers.py
@@ -6,6 +6,7 @@ from server.apps.category.models import Category
 class CategoryReadSerializer(serializers.ModelSerializer):
     """Serializer for Reading Categories."""
     main_category = serializers.SerializerMethodField()
+    sub_categories = serializers.SerializerMethodField()
 
     class Meta:
         """Meta definition for CategorySerializer."""
@@ -17,4 +18,9 @@ class CategoryReadSerializer(serializers.ModelSerializer):
     def get_main_category(self, obj):
         if obj.main_category:
             return {'id': obj.main_category.id, 'name': obj.main_category.name}
+        return None
+    
+    def get_sub_categories(self, obj):
+        if obj.main_category is None:
+            return CategoryReadSerializer(obj.sub_categories.all(), many=True).data
         return None
