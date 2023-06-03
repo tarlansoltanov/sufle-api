cccdeb9280d84ac930e7fb8b5d6e94c91b74eff8
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-03 20:17:38 +0200

    Rest Framework JWT added

--- server/settings/components/restframework.py
+++ server/settings/components/restframework.py
@@ -5,9 +5,31 @@ For more information, see the documentation:
 https://www.django-rest-framework.org/
 """
 
+import datetime
+
 from server.settings.components.common import INSTALLED_APPS
 
 INSTALLED_APPS += [
     "rest_framework",
     "drf_yasg"
 ]
+
+
+# REST framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
+
+# Simple JWT settings
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+}
