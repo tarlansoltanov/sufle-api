6a8fde9842c7cad4fc7fc02daaeee7a080cb64d3
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-03 12:37:06 +0200

    Category Read Only View added

--- server/apps/category/views.py
+++ server/apps/category/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+
+from server.apps.category.models import Category
+from server.apps.category.logic.serializers import CategoryReadSerializer
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    model = Category
+    serializer_class = CategoryReadSerializer
+    queryset = Category.objects.select_related('main_category').all()
+    permission_classes = [permissions.AllowAny]
