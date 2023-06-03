ca6e7b586c3756bec7a2f7f8d86bace64ba6a60c
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-03 14:28:53 +0200

    Category Model Admin modified'

--- server/apps/category/admin.py
+++ server/apps/category/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from server.apps.category.models import Category
@@ -7,8 +8,16 @@ from server.apps.category.models import Category
 class CategoryAdmin(admin.ModelAdmin):
     list_select_related = ['main_category']
     list_display = (
-        'id',
         'name',
+        'is_main',
         'main_category',
         'created_at',
     )
+
+    def is_main(self, obj):
+        return obj.main_category is None
+    
+    def get_form(self, request, obj, **kwargs):
+        form = super(CategoryAdmin,self).get_form(request, obj, **kwargs)
+        form.base_fields['main_category'] = forms.ModelChoiceField(queryset=Category.objects.filter(main_category=None))
+        return form
