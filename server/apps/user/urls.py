a5ece3f12fa10ce604c20ade4ff38fea7aa1f793
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-04 11:29:24 +0200

    User Logout and token refresh Views and URLs added

--- server/apps/user/urls.py
+++ server/apps/user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import LoginView, RegistrationView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import LoginView, RegistrationView, LogoutView
 
 app_name = "users"
 
 urlpatterns = [
     path("login", LoginView.as_view(), name="login"),
     path("register", RegistrationView.as_view(), name="register"),
+    path("logout", LogoutView.as_view(), name="logout"),
+    path("token-refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- server/apps/user/views.py
+++ server/apps/user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -109,3 +109,55 @@ class RegistrationView(APIView):
             {"message": "Invalid Credentials", "errors": serializer.errors},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+
+class LogoutView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "refresh": openapi.Schema(type=openapi.TYPE_STRING),
+            },
+        ),
+        responses={
+            205: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "message": openapi.Schema(
+                        type=openapi.TYPE_STRING, default="Logout successful"
+                    ),
+                },
+            ),
+            400: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "message": openapi.Schema(
+                        type=openapi.TYPE_STRING, default="Invalid token"
+                    ),
+                },
+            ),
+            401: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "message": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        default="Authentication credentials were not provided.",
+                    ),
+                },
+            ),
+        },
+    )
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(
+                {"message": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                {"message": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST
+            )
