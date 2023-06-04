ffea8102f3542b93ff4abef8f647099a1e85dedb
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-04 16:48:35 +0200

    Product and ProductImage models added and registered

--- server/apps/product/admin.py
+++ server/apps/product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_select_related = ("category",)
+    list_display = ("name", "category", "price", "created_at")
+    list_filter = ("category",)
+
+    inlines = [ProductImageInline]
--- server/apps/product/models.py
+++ server/apps/product/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """Model definition for Product."""
+
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey("category.Category", on_delete=models.CASCADE, related_name="products")
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Product."""
+
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        """Unicode representation of Product."""
+        return self.name
+
+
+class ProductImage(models.Model):
+    """Model definition for ProductImage."""
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
+    image = models.ImageField(upload_to="product")
+
+    class Meta:
+        """Meta definition for ProductImage."""
+
+        verbose_name = "ProductImage"
+        verbose_name_plural = "ProductImages"
+
+    def __str__(self):
+        """Unicode representation of ProductImage."""
+        return self.product.name
