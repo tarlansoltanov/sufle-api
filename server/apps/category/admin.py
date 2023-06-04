8e0d32ce1e1b1694d034e36696e5bb8dcc63d54d
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-04 11:40:10 +0200

    User Model Migration and Category Admin modified

--- server/apps/category/admin.py
+++ server/apps/category/admin.py
@@ -6,18 +6,20 @@ from server.apps.category.models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_select_related = ['main_category']
+    list_select_related = ["main_category"]
     list_display = (
-        'name',
-        'is_main',
-        'main_category',
-        'created_at',
+        "name",
+        "is_main",
+        "main_category",
+        "created_at",
     )
 
     def is_main(self, obj):
         return obj.main_category is None
-    
+
     def get_form(self, request, obj, **kwargs):
-        form = super(CategoryAdmin,self).get_form(request, obj, **kwargs)
-        form.base_fields['main_category'] = forms.ModelChoiceField(queryset=Category.objects.filter(main_category=None))
+        form = super(CategoryAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields["main_category"].queryset = Category.objects.filter(
+            main_category=None
+        )
         return form
