f9bcb636f1722bd6dd9813fb85d3cd3ca17e381b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-04 14:17:06 +0200

    Category Model validations added

--- server/apps/category/models.py
+++ server/apps/category/models.py
@@ -6,26 +6,32 @@ class Category(models.Model):
     """Model definition for Category."""
 
     name = models.CharField(max_length=255)
-    logo = models.FileField(upload_to='category/', blank=True, null=True)
+    logo = models.FileField(upload_to="category/", blank=True, null=True)
     main_category = models.ForeignKey(
-        'self',
+        "self",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        related_name='sub_categories'
+        related_name="sub_categories",
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         """Meta definition for Category."""
 
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         """Unicode representation of Category."""
         return self.name
-    
+
     def clean(self):
         if self.main_category is None and not bool(self.logo):
-            raise ValidationError('You can not create main category without logo')
+            raise ValidationError("You can not create main category without logo")
+
+        if self.main_category and self.main_category.main_category:
+            raise ValidationError("You can not create sub category for sub category")
+
+        if self.main_category and bool(self.logo):
+            raise ValidationError("You can not create sub category with logo")
