a67dd1202c23372a3f536c96f426ceb0db0968e3
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-08 20:52:55 +0200

    Banner Model added and registered to admin'

--- server/apps/announcement/admin.py
+++ server/apps/announcement/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Banner
 
-# Register your models here.
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ("id", "photo", "deadline", "created_at")
--- server/apps/announcement/models.py
+++ server/apps/announcement/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Banner(models.Model):
+    """Model definition for Banner."""
+
+    photo = models.ImageField(upload_to="banner/")
+    deadline = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Banner."""
+
+        verbose_name = "Banner"
+        verbose_name_plural = "Banners"
+
+    def __str__(self):
+        """Unicode representation of Banner."""
+        return f"{self.photo} - {self.deadline}"
