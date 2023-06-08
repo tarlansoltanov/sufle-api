2cad76ca58de5671c0df13ae7b1359228904d1f7
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-08 22:34:01 +0200

    Shop model added and registered

--- server/apps/shop/admin.py
+++ server/apps/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shop
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ("name", "address", "phone", "email", "working_hours", "map_url")
--- server/apps/shop/models.py
+++ server/apps/shop/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    """Model definition for Shop."""
+
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    working_hours = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="shop")
+    map_url = models.URLField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Shop."""
+
+        verbose_name = "Shop"
+        verbose_name_plural = "Shops"
+
+    def __str__(self):
+        """Unicode representation of Shop."""
+        return self.name
