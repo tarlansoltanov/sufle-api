e7d6e46dfc4a3309eea39641f7f180c657a37508
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-09 21:39:56 +0200

    Contact Model added and Registered

--- server/apps/contact/admin.py
+++ server/apps/contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "surname", "phone", "is_read", "created_at")
+    list_filter = ("is_read",)
+    search_fields = ("name", "surname", "phone", "message")
+    readonly_fields = ("name", "surname", "phone", "message")
+    actions = ("mark_as_read",)
+
+    def mark_as_read(self, request, queryset):
+        queryset.update(is_read=True)
+
+    mark_as_read.short_description = "Mark selected contacts as read"
--- server/apps/contact/models.py
+++ server/apps/contact/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Model definition for Contact."""
+
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Contact."""
+
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
+
+    def __str__(self):
+        """Unicode representation of Contact."""
+        return f"{self.name} {self.surname}"
