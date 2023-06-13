6fd0ed65d5b8fa97fb144f4fcc78805f01a4203a
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-13 15:08:48 +0200

    Vacancy and Requirements models added and registered

--- server/apps/vacancy/admin.py
+++ server/apps/vacancy/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vacancy, Requirement
+
+
+class RequirementInline(admin.TabularInline):
+    model = Requirement
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ("name", "title", "is_active", "created_at")
+    filter_fields = ("is_active",)
+    search_fields = ("name", "title")
+    inlines = [RequirementInline]
--- server/apps/vacancy/models.py
+++ server/apps/vacancy/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Vacancy(models.Model):
+    """Model definition for Vacancy."""
+
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="vacancy")
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Vacancy."""
+
+        verbose_name = "Vacancy"
+        verbose_name_plural = "Vacancies"
+
+    def __str__(self):
+        """Unicode representation of Vacancy."""
+        return self.name
+
+
+class Requirement(models.Model):
+    """Model definition for Requirement."""
+
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Requirement."""
+
+        verbose_name = "Requirement"
+        verbose_name_plural = "Requirements"
+
+    def __str__(self):
+        """Unicode representation of Requirement."""
+        return self.description
