2702fd92df0d73288d96a9104235f9c839a7a151
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-13 10:53:38 +0200

    Contact Model Type Field added and Registered

--- server/apps/contact/admin.py
+++ server/apps/contact/admin.py
@@ -5,10 +5,10 @@ from .models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ("name", "surname", "phone", "is_read", "created_at")
-    list_filter = ("is_read",)
+    list_display = ("name", "surname", "phone", "type", "is_read", "created_at")
+    list_filter = ("is_read", "type")
     search_fields = ("name", "surname", "phone", "message")
-    readonly_fields = ("name", "surname", "phone", "message")
+    readonly_fields = ("name", "surname", "phone", "message", "type")
     actions = ("mark_as_read",)
 
     def mark_as_read(self, request, queryset):
--- server/apps/contact/models.py
+++ server/apps/contact/models.py
@@ -4,9 +4,16 @@ from django.db import models
 class Contact(models.Model):
     """Model definition for Contact."""
 
+    TYPES_CHOICES = (
+        (0, "Şikayət"),
+        (1, "Təklif"),
+        (2, "Vakansiya"),
+    )
+
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
+    type = models.PositiveSmallIntegerField(choices=TYPES_CHOICES, default=1)
     message = models.TextField()
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
