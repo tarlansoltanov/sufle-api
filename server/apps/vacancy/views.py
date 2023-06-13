84d2a26047083c53220b71859f3060b69f22f545
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-13 15:21:58 +0200

    Vacancy ViewSet active endpoint added

--- server/apps/vacancy/views.py
+++ server/apps/vacancy/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
 
 from server.apps.core.pagination import CustomPagination
 
@@ -13,3 +14,11 @@ class VacancyViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = VacancyReadSerializer
     pagination_class = CustomPagination
     permission_classes = [permissions.AllowAny]
+
+    @action(detail=False, methods=["get"])
+    def active(self, request):
+        """Get active vacancies."""
+        queryset = self.get_queryset().filter(is_active=True)
+        page = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
