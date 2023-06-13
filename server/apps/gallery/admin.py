88d4e60ee49e57e0de07daad08662539431de037
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-13 12:07:32 +0200

    Gallery model added and registered

--- server/apps/gallery/admin.py
+++ server/apps/gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Gallery
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    search_fields = ("title",)
--- server/apps/gallery/models.py
+++ server/apps/gallery/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Gallery(models.Model):
+    """Model definition for Gallery."""
+
+    TYPE_CHOICES = (
+        ("image", "Image"),
+        ("video", "Video"),
+    )
+
+    title = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    file = models.FileField(upload_to="gallery")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Gallery."""
+
+        verbose_name = "Gallery"
+        verbose_name_plural = "Gallery Items"
+
+    def __str__(self):
+        """Unicode representation of Gallery."""
+        return self.title
