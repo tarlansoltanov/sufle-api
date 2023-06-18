adcceb39d6acc9b75e8d677474e284a2595975c7
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-18 15:01:50 +0200

    User Model OTP and OTP related functions added

--- server/apps/user/models.py
+++ server/apps/user/models.py
@@ -1,6 +1,11 @@
+import random
+
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .logic.managers import CustomUserManager
 
@@ -18,6 +23,9 @@ class User(AbstractUser):
     )
     birth_date = models.DateField(_("date of birth"), null=True, blank=True)
 
+    otp = models.CharField(_("otp"), max_length=6, null=True, blank=True)
+    otp_created_at = models.DateTimeField(_("otp created at"), null=True, blank=True)
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["phone"]
 
@@ -30,3 +38,23 @@ class User(AbstractUser):
         verbose_name = "User"
         verbose_name_plural = "Users"
         ordering = ["-id"]
+
+    def generate_otp(self):
+        self.otp = random.randint(100000, 999999)
+        self.otp_created_at = timezone.now()
+        self.save()
+        return self.otp
+
+    def verify_otp(self, otp):
+        return self.otp == otp if self.is_otp_valid() else False
+
+    def is_otp_valid(self):
+        return timezone.now() <= timezone.timedelta(minutes=5) + self.otp_created_at
+
+    def get_tokens(self):
+        refresh = RefreshToken.for_user(self)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
