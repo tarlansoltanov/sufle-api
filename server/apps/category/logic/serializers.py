81193c10e0a775fee9879d8e0180f56870279f0d
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-28 18:12:01 +0200

    Category Logo White, Red and Grey fields added

--- server/apps/category/logic/serializers.py
+++ server/apps/category/logic/serializers.py
@@ -1,6 +1,32 @@
 from rest_framework import serializers
 
+from drf_yasg.utils import swagger_serializer_method
+import drf_yasg.openapi as openapi
+
 from server.apps.category.models import Category
+
+
+class CategoryLogoSerializer(serializers.ModelSerializer):
+    """Serializer for Category Logo."""
+
+    white = serializers.SerializerMethodField()
+    red = serializers.SerializerMethodField()
+    grey = serializers.SerializerMethodField()
+
+    class Meta:
+        """Meta definition for CategoryLogoSerializer."""
+
+        model = Category
+        fields = ("white", "red", "grey")
+
+    def get_white(self, obj):
+        return obj.logo_white.url if obj.logo_white else None
+
+    def get_red(self, obj):
+        return obj.logo_red.url if obj.logo_red else None
+
+    def get_grey(self, obj):
+        return obj.logo_grey.url if obj.logo_grey else None
 
 
 class CategoryReadSerializer(serializers.ModelSerializer):
@@ -8,6 +34,7 @@ class CategoryReadSerializer(serializers.ModelSerializer):
 
     main_category = serializers.SerializerMethodField()
     sub_categories = serializers.SerializerMethodField()
+    logo = serializers.SerializerMethodField()
 
     class Meta:
         """Meta definition for CategorySerializer."""
@@ -32,4 +59,10 @@ class CategoryReadSerializer(serializers.ModelSerializer):
             return CategoryReadSerializer(
                 obj.sub_categories.all(), many=True, main=True
             ).data
+        return None
+
+    @swagger_serializer_method(serializer_or_field=CategoryLogoSerializer)
+    def get_logo(self, obj):
+        if obj.main_category is None:
+            return CategoryLogoSerializer(obj).data
         return None
--- server/apps/category/models.py
+++ server/apps/category/models.py
@@ -6,7 +6,9 @@ class Category(models.Model):
     """Model definition for Category."""
 
     name = models.CharField(max_length=255)
-    logo = models.FileField(upload_to="category/", blank=True, null=True)
+    logo_white = models.FileField(upload_to="category/", blank=True, null=True)
+    logo_red = models.FileField(upload_to="category/", blank=True, null=True)
+    logo_grey = models.FileField(upload_to="category/", blank=True, null=True)
     main_category = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
@@ -27,11 +29,19 @@ class Category(models.Model):
         return self.name
 
     def clean(self):
-        if self.main_category is None and not bool(self.logo):
-            raise ValidationError("You can not create main category without logo")
+        if self.main_category is None and (
+            not bool(self.logo_grey)
+            or not bool(self.logo_red)
+            or not bool(self.logo_white)
+        ):
+            raise ValidationError(
+                "You can not create main category without all the logos"
+            )
 
         if self.main_category and self.main_category.main_category:
             raise ValidationError("You can not create sub category for sub category")
 
-        if self.main_category and bool(self.logo):
-            raise ValidationError("You can not create sub category with logo")
+        if self.main_category and (
+            bool(self.logo_grey) or bool(self.logo_red) or bool(self.logo_white)
+        ):
+            raise ValidationError("You can not create sub category with any logo")
