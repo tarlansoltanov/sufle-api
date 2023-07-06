bb46c0f65cd76630ac5a80b3d7f0c0d28172f681
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-06 22:33:34 +0400

    Advert Model added and registered

--- server/apps/announcement/admin.py
+++ server/apps/announcement/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Banner
+from .models import Banner, Advert
 
 
 @admin.register(Banner)
 class BannerAdmin(admin.ModelAdmin):
     list_display = ("id", "photo", "deadline", "created_at")
+
+
+@admin.register(Advert)
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ("id", "photo", "title", "category", "created_at")
--- server/apps/announcement/models.py
+++ server/apps/announcement/models.py
@@ -17,3 +17,23 @@ class Banner(models.Model):
     def __str__(self):
         """Unicode representation of Banner."""
         return f"{self.photo} - {self.deadline}"
+
+
+class Advert(models.Model):
+    """Model definition for Advert."""
+
+    photo = models.ImageField(upload_to="advert/")
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey("category.Category", on_delete=models.CASCADE)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Advert."""
+
+        verbose_name = "Advert"
+        verbose_name_plural = "Adverts"
+
+    def __str__(self):
+        """Unicode representation of Advert."""
+        return f"{self.title} - {self.category}"
