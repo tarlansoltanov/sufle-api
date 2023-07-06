2de7785c0dda3f95b47b3cb17794d979b80256ce
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-06 22:48:14 +0400

    Advert Admin add and delete permissions added

--- server/apps/announcement/admin.py
+++ server/apps/announcement/admin.py
@@ -10,4 +10,14 @@ class BannerAdmin(admin.ModelAdmin):
 
 @admin.register(Advert)
 class AdvertAdmin(admin.ModelAdmin):
-    list_display = ("id", "photo", "title", "category", "created_at")
+    list_display = ("id", "title", "photo", "category", "created_at")
+    list_display_links = ("id", "title", "photo")
+
+    def has_add_permission(self, request):
+        num_objects = self.model.objects.count()
+        if num_objects >= 3:
+            return False
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
