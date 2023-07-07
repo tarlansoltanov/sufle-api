e840a1c9a4a5a097d2537bdf5e51fea2a84574c4
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-07 23:47:02 +0400

    Refresh Token Lifetime and Rotation added

--- server/settings/components/restframework.py
+++ server/settings/components/restframework.py
@@ -27,7 +27,9 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": datetime.timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=90),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 # CORS settings
