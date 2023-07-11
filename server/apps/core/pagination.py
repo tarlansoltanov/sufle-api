68ed6af97be40cfa19e4210208e2391572e0ef2d
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-11 19:05:13 +0400

    Pagination get all products added

--- server/apps/core/pagination.py
+++ server/apps/core/pagination.py
@@ -5,3 +5,8 @@ class CustomPagination(PageNumberPagination):
     page_size = 12
     page_size_query_param = "limit"
     max_page_size = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if request.query_params.get("limit") == "all":
+            return None
+        return super().paginate_queryset(queryset, request, view)
