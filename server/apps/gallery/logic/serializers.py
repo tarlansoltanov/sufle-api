5f9185e3e628df1b5b0a58a4b7c1af276763bb0a
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-19 15:58:18 +0400

    GallerySerializer Validation and Representation added

--- server/apps/gallery/logic/serializers.py
+++ server/apps/gallery/logic/serializers.py
@@ -3,22 +3,54 @@ from rest_framework import serializers
 from ..models import Gallery
 
 
-class GalleryReadSerializer(serializers.ModelSerializer):
+class GallerySerializer(serializers.ModelSerializer):
     """Serializer definition for Gallery."""
-
-    url = serializers.SerializerMethodField()
 
     class Meta:
         """Meta definition for GallerySerializer."""
 
         model = Gallery
-        fields = ("id", "title", "type", "url", "created_at")
+        fields = ("id", "title", "type", "file", "url", "modified_at", "created_at")
+
+        read_only_fields = ("id", "created_at")
+
         extra_kwargs = {
-            "title": {"required": False},
+            "file": {"write_only": True},
         }
 
-    def get_url(self, obj):
-        """Return URL for the file."""
-        if obj.type == "image":
-            return self.context["request"].build_absolute_uri(obj.file.url)
-        return obj.url
+    def to_representation(self, instance):
+        """Override to_representation method."""
+
+        data = super(GallerySerializer, self).to_representation(instance)
+        request = self.context.get("request")
+
+        if data["type"] == "image":
+            data["url"] = request.build_absolute_uri(instance.file.url)
+
+        return data
+
+    def validate(self, attrs):
+        """Override validate method."""
+
+        errors = {}
+
+        if attrs["type"] == "image":
+            if not attrs.get("file"):
+                if not self.instance or not self.instance.file.name:
+                    errors["file"] = "This field is required."
+
+            if attrs.get("url"):
+                errors["url"] = "This field should not be provided for image type."
+
+        elif attrs["type"] == "video":
+            if not attrs.get("url"):
+                if not self.instance or not self.instance.url:
+                    errors["url"] = "This field is required."
+
+            if attrs.get("file"):
+                errors["file"] = "This field should not be provided for video type."
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super().validate(attrs)
