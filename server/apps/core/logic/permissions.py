82e79dcd4b79cebd221a7ef62fa5967fe72b6e2b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-22 14:49:35 +0400

    New Custom Permissions added

--- server/apps/core/logic/permissions.py
+++ server/apps/core/logic/permissions.py
@@ -9,4 +9,31 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
+        return request.user and request.user.is_superuser
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    """Custom permission class to allow only staff users to edit an object."""
+
+    def has_permission(self, request, view):
+        """Return True if permission is granted."""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user and request.user.is_staff
+
+
+class IsAdmin(permissions.BasePermission):
+    """Custom permission class to allow only admin users to read and edit an object."""
+
+    def has_permission(self, request, view):
+        """Return True if permission is granted."""
+        return request.user and request.user.is_superuser
+
+
+class IsStaff(permissions.BasePermission):
+    """Custom permission class to allow only staff users to read and edit an object."""
+
+    def has_permission(self, request, view):
+        """Return True if permission is granted."""
         return request.user and request.user.is_staff
