d1c7e6c0d73380fea1b2097412d2296aa079b06b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-23 20:45:51 +0400

    Contac Serializer and ViewSet updated for read and delete with permissions

--- server/apps/contact/logic/serializers.py
+++ server/apps/contact/logic/serializers.py
@@ -3,9 +3,25 @@ from rest_framework import serializers
 from ..models import Contact
 
 
-class ContactWriteSerializer(serializers.ModelSerializer):
+class ContactSerializer(serializers.ModelSerializer):
     type = serializers.ChoiceField(choices=list(Contact.TYPES_CHOICES), default=1)
 
     class Meta:
         model = Contact
-        fields = ["name", "surname", "phone", "type", "message"]
+        fields = [
+            "id",
+            "name",
+            "surname",
+            "phone",
+            "type",
+            "message",
+            "created_at",
+        ]
+        read_only_fields = ["id", "created_at"]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        data["type"] = instance.get_type_display()
+
+        return data
--- server/apps/contact/views.py
+++ server/apps/contact/views.py
@@ -1,10 +1,27 @@
 from rest_framework import viewsets, permissions
 
-from .logic.serializers import ContactWriteSerializer
+from server.apps.core.logic.permissions import IsStaff
+
+from .models import Contact
+from .logic.serializers import ContactSerializer
 
 
-class ContactViewSet(viewsets.GenericViewSet, viewsets.mixins.CreateModelMixin):
+class ContactViewSet(viewsets.ModelViewSet):
     """ViewSet definition for Contact."""
 
-    serializer_class = ContactWriteSerializer
+    model = Contact
+    queryset = Contact.objects.all()
+
+    http_method_names = ["head", "options", "get", "post", "delete"]
+
+    serializer_class = ContactSerializer
+
     permission_classes = [permissions.AllowAny]
+
+    def get_permissions(self):
+        """Instantiates and returns the list of permissions that this view requires."""
+
+        if self.action == "create":
+            return [permissions.AllowAny()]
+
+        return [IsStaff()]
